b5a03ae8150950a92cfb6888694cff5f770f79ac
Ruan Failache <ruanfailache@gmail.com>
2022-05-01 19:15:50 -0300

    fix: make timer work

--- relogio-do-agora/src/components/Countdown/Clock.js
+++ relogio-do-agora/src/components/Countdown/Clock.js
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { renderClock } from './script';
-import { List, Title, ClockContainer } from './style';
-
-
+import React from 'react';
+import Timer from './script';
+import { ClockContainer, List, Title } from './style';
 
 const Clock = () => {
     return <ClockContainer>
         <Title>Relogio do Amanhã</Title>
 
         <List>
-            {renderClock()}
+            <Timer />
         </List>
+        
         <p>De acordo com o último <a target={"_blank"} href="https://www.ipcc.ch/site/assets/uploads/2019/07/SPM-Portuguese-version.pdf">relatório do IPCC</a>, o mundo inteiro precisa se unir e zerar, até 2050, as emissões de gás carbônico e reduzir pela metade as emissões de gás metano para previnir o desastre ambiental para o qual estamos nos dirigindo e termos a chance de amenizar os impactos das mudanças climáticas no planeta. Para termos essa chance, precisamos reduzir em, no mínimo, 43% as emissões de CO2 até 2030.</p>
         <p>O Relógio do Agora marca quanto tempo temos para agir e salvar nosso lar.</p>
         <p>Esse é um esforço coletivo, que depende não só da população, mas, principalmente de políticas públicas e ações da iniciativa privada. A curto prazo, a preservação ambiental não interessa ao mercado financeiro, e é vista como gasto. Mas o preço cobrado no futuro pela falta de ações hoje será catastrófico.</p>
--- relogio-do-agora/src/components/Countdown/script.js
+++ relogio-do-agora/src/components/Countdown/script.js
@@ -1,11 +1,25 @@
-import Countdown from "./countdown";
+import { useEffect, useMemo, useState } from "react";
+import Countdown from './countdown';
 import { Item } from "./style";
 
-export const finalDate = new Countdown("13 May 2050 20:00:00 GMT-0300")
+const FINAL_DATE = "13 May 2050 20:00:00 GMT-0300"
+const finalDateObject = new Date(FINAL_DATE)
 
-export const renderClock = () => {
-        
-    return finalDate.total.map((tempo) => {
-        return <Item>{tempo}</Item>
-    })
+const ONE_SECOND = 1000 // in ms
+
+export default function Timer () {
+    const [timer, setTimer] = useState(finalDateObject.getTime())
+    
+    const countdown = useMemo(() => {
+        const timerConverted2Date = new Date(timer)
+        return new Countdown(timerConverted2Date)
+    }, [timer])
+
+    useEffect(() => {
+        setInterval(() => {
+            setTimer((currTime) => currTime - 1)
+        }, ONE_SECOND)
+    }, [])
+
+    return countdown.total.map((value, index) => <Item key={index}>{value}</Item>)
 }
