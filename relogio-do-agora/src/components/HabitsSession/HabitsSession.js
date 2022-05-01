65ca783ca67ef0ca82acc29ea9e63b8072699f6f
Mariana Carolina de Souza <smcarolina42@gmail.com>
2022-05-01 16:22:02 -0300

    feat: create-habits-session

---
+++ relogio-do-agora/src/components/HabitsSession/HabitsSession.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { HabitContainer, SessionContainer } from "./style";
+
+const HabitsSession = () => {
+    return <SessionContainer>
+        
+        <HabitContainer>
+            Bons Hábitos
+        </HabitContainer>
+        <HabitContainer>
+            Maus Hábitos
+        </HabitContainer>
+        
+    </SessionContainer>
+} 
+
+export default HabitsSession
