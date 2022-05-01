f21b4d363369c5128ba4d986b378c44f438ac9d6
Mariana Carolina de Souza <smcarolina42@gmail.com>
2022-05-01 17:52:59 -0300

    feat: add img to header

--- relogio-do-agora/src/components/Header/Header.js
+++ relogio-do-agora/src/components/Header/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Link, Nav } from "./style";
+import { HeaderContainer, Link, Logo, Nav } from "./style";
+import LogoImg from '../../assets/img/LogoImg.jpeg'
 
 const Header = () => {
-    return <Nav>
+    return <HeaderContainer>
+        <Logo src={LogoImg}/>
+
+    <Nav>
             
         <Link href="">Início</Link>
-        <Link href="">Impactos</Link>
-        <Link href="">Como melhorar meus hábitos</Link>
         <Link href="">Quem somos</Link>
+        <Link href="">Ranking</Link>
+        <Link href="">Mude o relógio</Link>
     
     </Nav>
+    </HeaderContainer>
 }
 
 export default Header;
--- relogio-do-agora/src/components/Header/style.js
+++ relogio-do-agora/src/components/Header/style.js
@@ -1,10 +1,24 @@
 import styled from 'styled-components'
 
-export const Nav = styled.nav`
-    width: 80vw;
+export const HeaderContainer = styled.header`
+    width: 100%;
+    height: 10vh;
+    display: flex;
     border: 1px solid black;
+`
+
+export const Logo = styled.img`
+    height: 100%;
+    margin-left: 20px;
+`
+
+export const Nav = styled.nav`
+    width: 100%;
+    height: 100%;
+    
     display: flex;
     justify-content: center;
+    align-items: center;
 `
 
 export const Link = styled.a`
