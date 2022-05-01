4743ff64dd0084d237c7fd291652b440b156b68a
Mariana Carolina de Souza <smcarolina42@gmail.com>
2022-05-01 10:22:44 -0300

    feat: create header

---
+++ relogio-do-agora/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link, Nav } from "./style";
+
+const Header = () => {
+    return <Nav>
+            
+                <Link>Início</Link>
+                <Link>Quem somos</Link>
+                <Link>Entrar</Link>
+                <Link>Fazer cadastro</Link>
+            
+    </Nav>
+}
+
+export default Header;
