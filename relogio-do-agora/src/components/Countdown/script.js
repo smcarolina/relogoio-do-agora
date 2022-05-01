da9cae4390c1b408020fbe37c23ee11f2acfcebe
Mariana Carolina de Souza <smcarolina42@gmail.com>
2022-05-01 17:32:05 -0300

    fix: add description to clock session

--- relogio-do-agora/src/components/Countdown/script.js
+++ relogio-do-agora/src/components/Countdown/script.js
@@ -1,7 +1,7 @@
 import Countdown from "./countdown";
 import { Item } from "./style";
 
-export const finalDate = new Countdown("13 October 2032 20:00:00 GMT-0300")
+export const finalDate = new Countdown("13 May 2050 20:00:00 GMT-0300")
 
 export const renderClock = () => {
         
